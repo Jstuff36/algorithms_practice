442e17ca21bf5aad6b2f310098b531bfdcea779d
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-14 18:28:32 -0700

    Create a dynamic array class

--- Justin_white_alg1/lib/static_array.rb
+++ Justin_white_alg1/lib/static_array.rb
@@ -1,17 +1,18 @@
 # This class just dumbs down a regular Array to be statically sized.
 class StaticArray
+
   def initialize(length)
-    @length = length
+    @store = Array.new(length);
   end
 
   # O(1)
   def [](index)
-    self[index]
+    store[index]
   end
 
   # O(1)
   def []=(index, value)
-    self[index] = value
+    store[index] = value
   end
 
   protected

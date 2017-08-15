6061f08c1530ab11d6299000be4974cdef36d7ad
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-14 17:25:22 -0700

    Create static array class

--- Justin_white_alg1/lib/static_array.rb
+++ Justin_white_alg1/lib/static_array.rb
@@ -1,14 +1,17 @@
 # This class just dumbs down a regular Array to be statically sized.
 class StaticArray
   def initialize(length)
+    @length = length
   end
 
   # O(1)
   def [](index)
+    self[index]
   end
 
   # O(1)
   def []=(index, value)
+    self[index] = value
   end
 
   protected

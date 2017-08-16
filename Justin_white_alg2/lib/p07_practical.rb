a7a15ecfb494f73bb832f500692ce6d223df9911
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-16 10:35:05 -0700

    Use hash map just built to implement an algorithm to determine if a string can be rearranged to make a palindrome

--- Justin_white_alg2/lib/p07_practical.rb
+++ Justin_white_alg2/lib/p07_practical.rb
@@ -1,5 +1,15 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  my_hash_map = HashMap.new
+  string.chars.each do |let|
+    if my_hash_map.include?(let)
+      my_hash_map.delete(let)
+    else
+      my_hash_map.set(let, nil)
+    end
+  end
+
+  return false if my_hash_map.count > 1
+  true
 end

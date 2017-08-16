89eec9dbee6dd9d5d429607a30f185a67c6f94e6
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-15 18:04:52 -0700

    Create int set that resizes

--- Justin_white_alg2/lib/p01_int_set.rb
+++ Justin_white_alg2/lib/p01_int_set.rb
@@ -72,18 +72,26 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if count == num_buckets
+    self[num] << num
+    @count += 1
   end
 
   def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+    end
+    @count -= 1
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -91,5 +99,12 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    old_store.flatten.each do |el|
+      self.insert(el)
+    end
   end
+
 end

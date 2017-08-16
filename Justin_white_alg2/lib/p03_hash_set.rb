2effc690bb23210acbc0b6a6bbe5c32bc56f77fc
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-15 18:53:33 -0700

    Create hash set

--- Justin_white_alg2/lib/p03_hash_set.rb
+++ Justin_white_alg2/lib/p03_hash_set.rb
@@ -9,18 +9,26 @@ class HashSet
   end
 
   def insert(key)
+    resize! if count == num_buckets
+    self[key] << key
+    @count += 1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key].delete(key)
+    end
+    @count -= 1
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +36,11 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    old_store.flatten.each do |el|
+      self.insert(el)
+    end
   end
 end

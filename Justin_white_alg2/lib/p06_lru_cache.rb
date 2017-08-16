0487f4b79a6969ad617fd8ab01672d95604e3a72
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-16 10:30:37 -0700

    Create LRU cache

--- Justin_white_alg2/lib/p06_lru_cache.rb
+++ Justin_white_alg2/lib/p06_lru_cache.rb
@@ -15,6 +15,13 @@ class LRUCache
   end
 
   def get(key)
+    if map[key]
+      node = map[key]
+      update_node!(node)
+      node.val
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -23,14 +30,26 @@ class LRUCache
 
   private
 
+  attr_reader :store, :map
+
   def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    new_node = store.append(key, val)
+    map[key] = new_node
+
+    eject! if count > @max
+    val
   end
 
   def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+    node.remove
+    map[node.key] = store.append(node.key, node.val)
   end
 
   def eject!
+    old_node = store.first
+    old_node.remove
+    map.delete(old_node.key)
+    nil
   end
 end

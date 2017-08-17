29673796f8d9ae3c6177b717648c01fcea97c47b
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-17 09:16:27 -0700

    Find k largest elements in an array

--- Justin_white_alg3/lib/heap_sort.rb
+++ Justin_white_alg3/lib/heap_sort.rb
@@ -2,13 +2,13 @@ require_relative "heap"
 
 class Array
   def heap_sort!
-    0.upto(length - 1) do |idx|
+    1.upto(length - 1) do |idx|
       BinaryMinHeap.heapify_up(self, idx) { |el, el2| el <=> el2 }
     end
     (length - 1).downto(0) do |idx|
       self[0], self[idx] = self[idx], self[0]
       BinaryMinHeap.heapify_down(self, 0, idx) { |el, el2| el <=> el2 }
     end
+    reverse!
   end
-  reverse!
 end

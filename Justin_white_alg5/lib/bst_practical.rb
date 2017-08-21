6521a02ca4c646bfc8251bbe73befffd8bbc12c7
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-20 19:00:57 -0700

    kth largest not quite working. Cant figure out why

--- Justin_white_alg5/lib/bst_practical.rb
+++ Justin_white_alg5/lib/bst_practical.rb
@@ -1,3 +1,15 @@
+require_relative 'binary_search_tree'
 
 def kth_largest(tree_node, k)
+  arr = []
+  in_order_traversal(tree_node, arr, k)
+  arr.last
+end
+
+def in_order_traversal(tree_node, arr, k)
+  return if tree_node.nil?
+  return if arr.length == k - 1
+  in_order_traversal(tree_node.left, arr, k)
+  arr.push(tree_node)
+  in_order_traversal(tree_node.right, arr, k)
 end

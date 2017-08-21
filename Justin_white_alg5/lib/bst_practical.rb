81f5fd902a7d583cebe5f093874eccaeb9e27830
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-21 09:17:31 -0700

    Change name of traversal to reverse to make naming more concise

--- Justin_white_alg5/lib/bst_practical.rb
+++ Justin_white_alg5/lib/bst_practical.rb
@@ -15,14 +15,14 @@ require_relative 'binary_search_tree'
 
 def kth_largest(tree_node, k)
   arr = []
-  in_order_traversal(tree_node, arr, k)
+  reverse_order_traversal(tree_node, arr, k)
   arr[k-1]
 end
 
-def in_order_traversal(tree_node, arr, k)
+def reverse_order_traversal(tree_node, arr, k)
   return if tree_node.nil?
-  in_order_traversal(tree_node.right, arr, k)
+  reverse_order_traversal(tree_node.right, arr, k)
   arr.push(tree_node)
-  in_order_traversal(tree_node.left, arr, k)
+  reverse_order_traversal(tree_node.left, arr, k)
   return arr if arr.length == k
 end

0eb1a241d869c251b79d0ae5d03436dceab827d6
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-21 21:32:40 -0700

    Create topological sort

--- Justin_white_alg6/lib/topological_sort.rb
+++ Justin_white_alg6/lib/topological_sort.rb
@@ -3,5 +3,21 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
-
+  queue = []
+  sorted = []
+  vertices.each do |vertice|
+    queue << vertice if vertice.in_edges.empty?
+  end
+  until queue.empty?
+    current = queue.shift
+    sorted << current
+    until current.out_edges.empty?
+      edge = current.out_edges.pop
+      queue.push(edge.to_vertex) if edge.to_vertex.in_edges.length == 1
+      edge.destroy!
+    end
+    # edges.each {|edge| edge.destroy! }
+  end
+  return [] unless sorted.length == vertices.length
+  sorted
 end

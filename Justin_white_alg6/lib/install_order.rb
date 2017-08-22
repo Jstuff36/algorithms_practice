9afb645b6f4767bdcfe30352c9ea81334617dcb1
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-22 11:18:23 -0700

    Complete install order

--- Justin_white_alg6/lib/install_order.rb
+++ Justin_white_alg6/lib/install_order.rb
@@ -7,8 +7,25 @@
 
 # Import any files you need to
 
-
+require_relative 'graph'
+require_relative 'topological_sort'
 
 def install_order(arr)
+  max_id = arr.flatten.max
+  ids = (1..max_id).to_a
+
+  vertices = {}
+  arr.each do |file|
+    vertices[file[0]] = Vertex.new(file[0]) unless vertices[file[0]]
+    vertices[file[1]] = Vertex.new(file[1]) unless vertices[file[1]]
+    Edge.new(vertices[file[1]], vertices[file[0]])
+  end
+
+  no_dependencies = []
+  ids.each do |id|
+    no_dependencies << id if vertices[id].nil?
+  end
+
+  no_dependencies + topological_sort(vertices.values).map { |value| value.value }
 
 end
